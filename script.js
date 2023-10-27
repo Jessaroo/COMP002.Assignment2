e137b02e6670c1b1c1277acea4656918fe56c709
Jessica <j_caetana@yahoo.com>
2023-10-27 16:32:13 -0700

    first

---
+++ script.js
@@ -0,0 +1,56 @@
+// For each of the 10 tasks below, write a console.log(...) statement that satisfies the
+// requirement. Write your code to accomplish the programming task beneath each comment.
+
+// 1. Write code to output the following string to the console:
+// "Hello World!"
+let variable1 = "Hello World!";
+console.log(variable1);
+
+// 2. Write code to concatenate three strings and output the result to the console:
+// "Hello", " ", "World!"
+let variable2 = "Hello" + " " + "World!";
+console.log(variable2);
+
+// 3. Write code to output "Hello" on one line and "World!" on the next line to the console.
+// *Note: You should only have one console.log() statement - how do you add a new line in
+// a string?...
+let variable3 = "Hello";
+console.log(variable3);
+variable3 = "World!";
+console.log(variable3);
+
+// 4. Write code to output the result of the arithmetic expression to the console:
+// 3 * 8 + 2;
+let variable4 = 3 * 8 + 2;
+console.log(variable4);
+
+// 5. Write code to output the result of the comparison:
+// 5 is less than 10.
+let variable5 = 5 < 10;
+console.log(variable5);
+
+// 6. Write code to output the result of the comparison:
+// 10 is greater than 5.
+let variable6 = 10 > 5;
+console.log(variable6);
+
+// 7. Write code to output the result of the comparison:
+// "Apples" is NOT equal to "Oranges"
+let variable7 = "apples" != "oranges";
+console.log(variable7);
+
+// 8. Write code to output the result of the comparison:
+// 10 * 10 is greater than 50 AND 5 * 5 is less than 50
+let variable8 = 10 * 10 > 50 && 5 * 5 < 50;
+console.log(variable8);
+
+// 9. Write code to output the result of the comparison:
+// 1 + 2 is greater than 5 OR 1 * 10 is greater than 5
+let variable9 = 1 + 2 > 5 || 1 * 10 > 5;
+console.log(variable9);
+
+// 10. Write code that demonstrates an automatic type conversion as shown in Chapter 01.
+let variable10 =10;
+console.log(variable10);
+variable10 = "15";
+console.log(variable10);
